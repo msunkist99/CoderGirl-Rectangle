3ef9632b46e36cffee1999710787ef5545a2de8e
Scott Kuhl <scott@kuhl.ws>
2018-08-31 23:42:56 -0500

    Initial project.

--- Rectangle/Program.cs
+++ Rectangle/Program.cs
@@ -2,11 +2,21 @@
 
 namespace Rectangle
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter the length of the rectangle: ");
+
+            // Get input from the user and save it to a variable.
+
+            Console.Write("Enter the width of the rectangle: ");
+
+            // Get input from the user and save it to a second variable.
+
+            // Output - The area of the rectangle is (computed value)
+
+            Console.ReadLine();
         }
     }
 }
