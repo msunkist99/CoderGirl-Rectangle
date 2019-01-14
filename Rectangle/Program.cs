3e5ee0871e14f2567334d6acff89844cbddca383
msunkist99 <msunkist99@gmail.com>
2019-01-14 17:44:10 -0600

    prompt for length and width print rectangle area to console.

--- Rectangle/Program.cs
+++ Rectangle/Program.cs
@@ -9,14 +9,20 @@ namespace Rectangle
             Console.Write("Enter the length of the rectangle: ");
 
             // Get input from the user and save it to a variable.
+            double length = double.Parse(Console.ReadLine());
 
             Console.Write("Enter the width of the rectangle: ");
 
             // Get input from the user and save it to a second variable.
+            double width = double.Parse(Console.ReadLine());
 
             // Output - The area of the rectangle is (computed value)
+            Console.WriteLine("Area of the rectangle is --- " + (length * width));
 
             Console.ReadLine();
+
+            //  NOTE there is no try/catch logic here to validate input.  
+            //  we are trusting the user to enter only numeric data.
         }
     }
 }
